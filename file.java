dba77c112eb4beda3f85ce92f325b1453a2f1765
Nandan H S <ngowda7779@gmail.com>
2026-01-21 06:25:10 +0000

    not

---
+++ file.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        
+        // Simple variables
+        int number = 42;
+        String message = "Java is awesome";
+        double pi = 3.14159;
+        
+        System.out.println("Number: " + number);
+        System.out.println("Message: " + message);
+        System.out.println("Pi: " + pi);
+        
+        // Simple loop
+        System.out.println("\nNumbers from 1 to 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
